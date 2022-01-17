c9cfe54aa687140b69450c6f67f9fdfa96d19269
Ryota Kameoka <ok@ryota-ka.me>
2022-01-18 01:08:57 +0900

    Implement `<TableOfContents />` component

---
+++ src/components/TableOfContents/index.tsx
@@ -0,0 +1,36 @@
+import type * as Post from '../../Post';
+
+type Props = {
+    className?: string;
+    sections: Post.TableOfContents.T;
+};
+
+const TableOfContents: React.FC<Props> = ({ className = '', sections }) => {
+    const items = sections.flatMap(({ href, subsections, title }) => [
+        { title, href, sub: false },
+        ...subsections.map(({ href, title }) => ({ href, title, sub: true })),
+    ]);
+
+    return (
+        <div className={`${className} bg-zinc-50 dark:bg-zinc-900 rounded-xl p-4 shadow`}>
+            <div className="text-lg mb-3 font-bold">Table of Contents</div>
+            <ul className="space-y-1">
+                {items.map(({ title, href, sub }) => (
+                    <li key={href}>
+                        <a
+                            href={href}
+                            className={
+                                'text-gray-600 dark:text-gray-300 block line-clamp-1 hover:text-sky-700 dark:hover:text-amber-500 ' +
+                                (sub ? 'ml-4' : '')
+                            }
+                        >
+                            {title}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export { TableOfContents };
