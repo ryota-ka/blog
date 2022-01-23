8eddf77efe4937fe13651dc98bbbb64c3f17cc6a
Ryota Kameoka <ok@ryota-ka.me>
2022-01-23 23:07:07 +0900

    Define `brightness-25` class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx', './src/custom-elements/**/*.ts'],
     theme: {
-        extend: {},
+        extend: {
+            brightness: {
+                25: '.25',
+            },
+        },
     },
     plugins: [require('@tailwindcss/line-clamp')],
 };
