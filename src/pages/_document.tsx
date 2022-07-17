be5869ac865b87a497d9cc06b24d50395705cbf4
Ryota Kameoka <ok@ryota-ka.me>
2022-07-17 18:38:10 +0900

    Set `scroll-padding-top` to `<html>` element

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
     public render(): React.ReactElement {
         return (
-            <Html lang="ja">
+            <Html lang="ja" className="scroll-pt-8">
                 <Head />
                 <body className="bg-zinc-100 dark:bg-black dark:text-zinc-50 pb-12">
                     <Main />
